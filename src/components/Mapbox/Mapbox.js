03b379d42a0183a6dd073730076d5e99fe5631f5
Viet Anh Le <mc.vietanh@gmail.com>
2022-07-21 13:44:49 -0400

    done q4

--- src/components/Mapbox/Mapbox.js
+++ src/components/Mapbox/Mapbox.js
@@ -1,79 +1,25 @@
 import * as React from 'react';
 import { useRef } from 'react';
 import { Map, Source, Layer } from 'react-map-gl';
-// import 'mapbox-gl/dist/mapbox-gl.css';
 import './Mapbox.css';
 import {
   clusterLayer,
   clusterCountLayer,
   unclusteredPointLayer,
+  unclusteredLabelLayer,
 } from './layers';
 
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoibWl6dW5la284NiIsImEiOiJjazYxbmhzcXowNWoyM21zY2lpemRtMTJwIn0.zNhaJaXle_1Yssue-lXhNQ'; // Set your mapbox token here
+  'pk.eyJ1IjoiZmVsaXhsZSIsImEiOiJja21kd2ZqaDUycTFjMnZxcnpjZTA1YTlsIn0.XQWrn5uNVtuBJO-LHBq48A'; // Set your mapbox token here
 
-export default function Mapbox() {
+export default function Mapbox({ data }) {
   const mapRef = useRef(null);
-
-  const myGeoJSON = {};
-  myGeoJSON.type = 'FeatureCollection';
-  myGeoJSON.crs = {
-    type: 'name',
-    properties: { name: 'urn:ogc:def:crs:OGC:1.3:CRS84' },
-  };
-  myGeoJSON.features = [
-    {
-      type: 'Feature',
-      properties: {
-        id: 'ak16994521',
-        mag: 2.3,
-        time: 1507425650893,
-        felt: null,
-        tsunami: 0,
-      },
-      geometry: { type: 'Point', coordinates: [-79.3899, 43.6708, 0.0] },
-    },
-    {
-      type: 'Feature',
-      properties: {
-        id: 'ak16994519',
-        mag: 1.7,
-        time: 1507425289659,
-        felt: null,
-        tsunami: 0,
-      },
-      geometry: { type: 'Point', coordinates: [-79.3871, 43.6426, 105.5] },
-    },
-
-    {
-      type: 'Feature',
-      properties: {
-        id: 'ak16994517',
-        mag: 1.6,
-        time: 1507424832518,
-        felt: null,
-        tsunami: 0,
-      },
-      geometry: { type: 'Point', coordinates: [-79.0849, 43.0896, 105.5] },
-    },
-    {
-      type: 'Feature',
-      properties: {
-        id: 'ci38021336',
-        mag: 1.42,
-        time: 1507423898710,
-        felt: null,
-        tsunami: 0,
-      },
-      geometry: { type: 'Point', coordinates: [-123.0884, 49.2965, 7.64] },
-    },
-  ];
 
   const onClick = (event) => {
     const feature = event.features[0];
     const clusterId = feature.properties.cluster_id;
 
-    const mapboxSource = mapRef.current.getSource('earthquakes');
+    const mapboxSource = mapRef.current.getSource('locations');
 
     mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
       if (err) {
@@ -101,11 +47,12 @@ export default function Mapbox() {
         interactiveLayerIds={[clusterLayer.id]}
         onClick={onClick}
         ref={mapRef}
+        cursor='pointer'
       >
         <Source
-          id='earthquakes'
+          id='locations'
           type='geojson'
-          data={myGeoJSON}
+          data={data}
           cluster={true}
           clusterMaxZoom={14}
           clusterRadius={50}
@@ -113,6 +60,8 @@ export default function Mapbox() {
           <Layer {...clusterLayer} />
           <Layer {...clusterCountLayer} />
           <Layer {...unclusteredPointLayer} />
+
+          <Layer {...unclusteredLabelLayer} />
         </Source>
       </Map>
     </div>
--- src/components/Mapbox/layers.js
+++ src/components/Mapbox/layers.js
@@ -1,35 +1,58 @@
 export const clusterLayer = {
   id: 'clusters',
   type: 'circle',
-  source: 'earthquakes',
+  source: 'locations',
   filter: ['has', 'point_count'],
   paint: {
-    'circle-color': ['step', ['get', 'point_count'], '#51bbd6', 100, '#f1f075', 750, '#f28cb1'],
-    'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40]
-  }
+    'circle-color': [
+      'step',
+      ['get', 'point_count'],
+      '#51bbd6',
+      100,
+      '#f1f075',
+      750,
+      '#f28cb1',
+    ],
+    'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
+  },
 };
 
 export const clusterCountLayer = {
   id: 'cluster-count',
   type: 'symbol',
-  source: 'earthquakes',
+  source: 'locations',
   filter: ['has', 'point_count'],
   layout: {
     'text-field': '{point_count_abbreviated}',
     'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-    'text-size': 12
-  }
+    'text-size': 12,
+  },
 };
 
 export const unclusteredPointLayer = {
   id: 'unclustered-point',
   type: 'circle',
-  source: 'earthquakes',
+  source: 'locations',
   filter: ['!', ['has', 'point_count']],
   paint: {
     'circle-color': '#11b4da',
     'circle-radius': 4,
     'circle-stroke-width': 1,
-    'circle-stroke-color': '#fff'
-  }
+    'circle-stroke-color': '#fff',
+  },
+};
+
+export const unclusteredLabelLayer = {
+  id: 'unclustered-label',
+  type: 'symbol',
+  source: 'locations',
+  filter: ['!', ['has', 'point_count']],
+  layout: {
+    'text-field': 'Flag',
+    'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+    'text-size': 12,
+  },
+  paint: {
+    'text-color': 'cyan',
+  },
 };
